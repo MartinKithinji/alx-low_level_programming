04612b7529f867cb047fbf70e010d4c45f9bdb70
Martin Kithinji <mwimsae@gmail.com>
2022-09-12 13:45:36 -0700

    Script 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,10 @@ int main(void)
 
 {
 
-	write(STDOUT_FILENO, "and that piece of art is useful
+		write(STDOUT_FILENO, "and that piece of art is useful
 
-			\" - Dora Korpar, 2015-10-19\n", 59);
+							\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (1);
+			return (1);
 
 }
