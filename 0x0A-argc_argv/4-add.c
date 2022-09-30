bc60ad2505bceaf24c38f806167cebf17d298c20
Martin Kithinji <mwimsae@gmail.com>
2022-09-30 13:47:18 -0700

    Script 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,43 +13,33 @@
 int main(int argc, char *argv[])
 
 {
-
 	int i, j, sum;
-
-
 
 	if (argc == 1)
 
 	{
-
 		printf("0\n");
 
 		return (0);
-
 	}
 
 	for (i = 1; i < argc; i++)
 
 	{
-
 		for (j = 0; argv[i][j] != '\0'; j++)
 
 		{
 
-			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
+		if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
 
 			{
-
 				printf("Error\n");
 
 				return (1);
-
 			}
-
 		}
 
 		sum += atoi(argv[i]);
-
 	}
 
 	printf("%d\n", sum);
