3b3b32e35c6072f85d8b11d91222a74c863a1845
Martin Kithinji <mwimsae@gmail.com>
2022-10-06 13:36:45 -0700

    Script 101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,23 +12,26 @@ int _isNum(char *num)
 	int i;
 
 	for (i = 0; num[i] != '\0'; i++)
-	{		if (num[i] < '0' || num[i] > '9')
+	{
+		if (num[i] < '0' || num[i] > '9')
 		return (0);
 	}
 	return (1);
 }
 /**
- *_memset - copies a character to the firstn characters of the string pointed
- *  @s: original string
- *  @b: value to remplace
- *  @n: number of bytes
- *  Return: s (string modify)
+ * _memset - copies a character to the firstn characters of the string pointed
+ * @s: original string
+ * @b: value to remplace
+ * @n: number of bytes
+ * Return: s (string modify)
  */
 char *_memset(char *s, char b, unsigned int n)
-{	unsigned int i;
+{
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
+
 	return (s);
 }
 /**
@@ -57,25 +60,25 @@ int main(int argc, char *argv[])
 	int length, c, prod, i, j, l1, l2;
 
 	int *res;
-	
+
 	if ((argc != 3 || !(_isNum(argv[1]))) || !(_isNum(argv[2])))
 		puts("Error"), exit(98);
 	l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
+
 	length = l1 + l2;
+
 	res = calloc(length, sizeof(int *));
 	if (res == NULL)
 		puts("Error"), exit(98);
 	for (i = l2 - 1; i > -1; i--)
 	{
 		c = 0;
+
 		for (j = l1; j > -1; j--)
 		{
-			prod = (argv[2][i] - '0') * (argv[1][j] - '0');
-
+		prod = (argv[2][i] - '0') * (argv[1][j] - '0');
 			c = (prod / 10);
-
 			res[(i + j) + 1] += (prod % 10);
-
 			if (res[(i + j) + 1] > 9)
 			{
 				res[i + j] += res[(i + j) + 1] / 10;
@@ -85,7 +88,6 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (res[0] == 0)
-
 		i = 1;
 	else
 		i = 0;
