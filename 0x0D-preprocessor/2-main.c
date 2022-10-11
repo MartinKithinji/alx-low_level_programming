41f1437acd562e70c073273bba1d503fe813e277
Martin Kithinji <mwimsae@gmail.com>
2022-10-11 14:44:07 -0700

    Script all mp

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+/**
+ * main - print the the name of the file it was compiled from,
+ * followed by a new line
+ * Return: 0 on succes
+ */
+
+int main(void)
+
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
