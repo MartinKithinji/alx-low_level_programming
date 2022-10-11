51713a651e4d2d61916979dd3cef3cb89d183072
Martin Kithinji <mwimsae@gmail.com>
2022-10-11 12:02:51 -0700

    Script 1-init_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,29 @@
 #include <stddef.h>
+
 #ifndef DOG_H
+
 #define DOG_H
 
 /**
- * struct dog - contains dog information
+ * _struct dog - contains dog information
  *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ *  @name: name of the dog
+ *  @age: age of the dog
+ *  @owner: owner of the dog
  *
- * Description: hold all data needed for dog
+ *  Description: hold all data needed for dog
  */
+
 struct dog
+
 {
-	char *name;
 
-	float age;
+char *name;
 
-	char *owner;
+float age;
+
+char *owner;
+
 };
 
 /**
