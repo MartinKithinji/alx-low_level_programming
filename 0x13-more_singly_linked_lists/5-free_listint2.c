ed59476c44684fe5b212f32d880e59e7f4f6ee25
Martin Kithinji <mwimsae@gmail.com>
2022-10-27 16:02:23 -0700

    all

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees the memory used before and sets head to NULL
+ * @head: The head node
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+
+{
+
+	listint_t *tmp;
+
+
+
+	if (!head)
+
+		return;
+
+	while (*head)
+
+	{
+
+		tmp = *head;
+
+		*head = (*head)->next;/*Needed () cuz syntaxis*/
+
+		free(tmp);
+
+	}
+
+	*head = NULL;
+
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deleted the first node and returns its value
+ * @head: The head node
+ * return: @val the value of that head
+ */
+
+int pop_listint(listint_t **head)
+
+{
+
+	listint_t *tmp;
+
+	int val = 0;
+
+
+
+	if (*head == NULL)
+
+		return (0);
+
+	tmp = (*head)->next;/*Needed () cuz syntaxis*/
+
+	val = (*head)->n;
+
+	free(*head);
+
+	*head = tmp;
+
+	return (val);
+
+}
