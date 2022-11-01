34f326772b71532fbb0dd4b4b2cde60c6497b8ad
Martin Kithinji <mwimsae@gmail.com>
2022-11-01 13:29:53 -0700

    0-binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,82 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - A finction that converts binary number
- * to an unsigned integer
- * @b: A pointer to binary string
- * Return: unsigned integer converted number
+ * _strlen - finding the lenght of a string.
+ * @s: first pointer
+ * Return: it returns the length of the string.
+ */
+
+int _strlen(char *s)
+
+{
+	int str;
+
+	str = 0;
+
+	while (s[str] != '\0')
+
+		str++;
+
+	return (str);
+}
+
+/**
+ * x_to_the_n - Fucntion that does to the power of
+ * @x: the number
+ * @n: the eleveado
+ * Return:@number the result
+ */
+
+int x_to_the_n(int x, int n)
+
+{
+	int i;
+
+	int number = 1;
+
+	for (i = 0; i < n; ++i)
+
+		number *= x;
+
+	return (number);
+
+}
+
+/**
+ * binary_to_uint - Converts binary to decimals
+ * @b: pointer to char given by main
+ * Return:@result the result
  */
 
 unsigned int binary_to_uint(const char *b)
-
 {
-	unsigned int base = 1, result = 0, len = 0;
+	int j = 0, operand = 0, len = 0;
 
-	if (b == NULL)
+	unsigned int result = 0;
+
+	if (!b)
 
 		return (0);
 
-	while (b[len])
+	len = _strlen((char *) b);
 
-		len++;
-
-	while (len)
+	while (b[j] != '\0')
 
 	{
 
-		if (b[len - 1] != '0' && b[len - 1] != '1')
+		if (b[j] < 48 || b[j] > 49)
 
 			return (0);
 
+		operand = x_to_the_n(2, j);
 
+		if (b[len - 1] == 49)/*Ascci code of 1*/
 
-		if (b[len - 1] == '1')
-
-			result += base;
-
-		base *= 2;
+			result += operand;
 
 		len--;
+
+		j++;
 
 	}
 
